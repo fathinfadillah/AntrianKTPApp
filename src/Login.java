527c6e993877184aeec5334cad4f2007b1c23b9c
Muhammad Fathin Fadillah <0320200016@polman.astra.ac.id; m.fathinfadillah@gmail.com>
2021-06-14 14:46:05 +0700

    Update

--- src/Login.java
+++ src/Login.java
@@ -1,8 +1,19 @@
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class Login {
-    private JTextField textField1;
-    private JTextField textField2;
-    private JButton LOGINButton;
-    private JButton SIGNUPButton;
+    private JTextField txtUsername;
+    private JTextField txtPassword;
+    private JButton btnLogin;
+    private JButton btnSignUp;
+
+    public Login() {
+        btnLogin.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+    }
 }
