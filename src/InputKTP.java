36f8c09f2b94621fa83bb22859da338bd57747f6
Aris Dwi Hanggoro <0320200004@polman.astra.ac.id>
2021-06-14 17:31:37 +0700

    Penambahan Design Form Input KTP

---
+++ src/InputKTP.java
@@ -0,0 +1,12 @@
+import javax.swing.*;
+
+public class InputKTP {
+    private JPanel JPInputKTP;
+    private JPanel JPLeftMenu;
+    private JPanel JPRightMenu;
+    private JButton berandaButton;
+    private JButton daftarAntrianButton;
+    private JButton SIMPANButton;
+    private JTextField textField1;
+    private JComboBox comboBox1;
+}
