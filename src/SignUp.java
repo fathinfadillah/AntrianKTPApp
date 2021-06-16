b43a59536a9a2e2c2ca40b2ef6a2c0b890b22a17
Muhammad Fathin Fadillah <0320200016@polman.astra.ac.id; m.fathinfadillah@gmail.com>
2021-06-16 23:16:51 +0700

    Add SignUp Form

---
+++ src/SignUp.java
@@ -0,0 +1,11 @@
+import javax.swing.*;
+
+public class SignUp {
+    private JPanel SignUp;
+    private JPanel panel2;
+    private JTextField txtNama;
+    private JTextField txtRole;
+    private JButton btnSignUp;
+    private JTextField txtUsername;
+    private JTextField txtPassword;
+}
